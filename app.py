5bd6ea0a1723736338bf085c5ea3ede1a010a03b
Nick Fratto <nfratto3@gatech.edu>
2021-04-20 21:18:35 -0400

    added search bar and cleaned up flask calls

--- app.py
+++ app.py
@@ -7,13 +7,12 @@ app = Flask(__name__)
 #############################
 @app.route('/')
 def home_page():
-    example_embed='This string is from python'
-    return render_template('index.html', embed=example_embed)
+    return render_template('index.html')
 
 @app.route('/test', methods=['GET', 'POST'])
 def testfn():    # GET request
     if request.method == 'GET':
-        message = {'greeting':'Hello from Flask!'}
+        message = {'greeting':'Hello from Python Flask!'}
         return jsonify(message)  # serialize and use JSON headers    # POST request
     if request.method == 'POST':
         print(request.get_json())  # parse as JSON
